13b4432065b594d940ebea7cd8567653595c06ee
frolleks <frolleks@hotmail.com>
2023-12-26 15:16:46 +0700

    refactor: rework installer

--- scripts/installer.js
+++ scripts/installer.js
@@ -8,27 +8,8 @@ const windowsDiscordPaths = {
 
 function installFrogcord() {
   const resourcesPath = path.join(windowsDiscordPaths.stable, "resources");
-  const appPath = path.join(resourcesPath, "app");
+  const appPath = path.join(resourcesPath, "app.asar");
 
-  // Ensure the 'app' directory exists
-  if (!fs.existsSync(appPath)) {
-    fs.mkdirSync(appPath, { recursive: true });
-  }
-
-  // Read and write files synchronously
-  const indexPath = path.join(appPath, "index.js");
-  const packagePath = path.join(appPath, "package.json");
-  let patcherPath = path.resolve("dist/patcher.js");
-
-  patcherPath = patcherPath.replace(/\\/g, "\\\\");
-
-  fs.writeFileSync(indexPath, `require("${patcherPath}")`);
-  fs.writeFileSync(
-    packagePath,
-    '{\n  "name": "discord",\n  "main": "index.js"\n}'
-  );
-
-  // Kill Discord process
   exec("taskkill /f /im discord.exe", (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error.message}`);
@@ -38,8 +19,24 @@ function installFrogcord() {
       console.error(`Stderr: ${stderr}`);
       return;
     }
-    console.log(`Stdout: ${stdout}`);
+    console.log("Please relaunch Discord.");
   });
+
+  if (fs.existsSync(appPath)) {
+    fs.renameSync(appPath, "_app.asar");
+    fs.mkdirSync(appPath, { recursive: true });
+  }
+
+  // Read and write files synchronously
+  const indexPath = path.join(appPath, "index.js");
+  const packagePath = path.join(appPath, "package.json");
+  const patcherPath = path.resolve("dist/patcher.js").replace(/\\/g, "\\\\");
+
+  fs.writeFileSync(indexPath, `require("${patcherPath}")`);
+  fs.writeFileSync(
+    packagePath,
+    '{\n  "name": "discord",\n  "main": "index.js"\n}'
+  );
 }
 
 installFrogcord();
